ce22b23e8d45753a5fdd68d15c3a18960ae03195
CORAUD\ktroufleau <ktroufleau@cdh-it.com>
2017-11-10 15:42:18 +0100

    error management and insertion in POST

--- flappy_api.py
+++ flappy_api.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
+import sys
 
 app = Flask(__name__)
 
@@ -13,10 +14,15 @@ returns the <num_score> highest scores
 @app.route('/flappy_api/getscore/<int:num_score>', methods=['GET'])
 def get_n_score(num_score):
     l_score = []
-    cursor = mongo.db.flappy_score.find({}).sort([("score",-1)]).limit(num_score)
-    for score in cursor:
+    try:
+        cursor = mongo.db.flappy_score.find({}).sort([("score",-1)]).limit(num_score)
+        for score in cursor:
             l_score.append(score)
-    return dumps(l_score)
+        response = make_response(dumps(l_score), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    except:
+        return make_response (jsonify({'error':'the database doesn \'t appear to be available'}), 504)
 
 """
 returns all scores 
@@ -24,22 +30,36 @@ returns all scores
 @app.route('/flappy_api/getscore', methods=['GET'])
 def get_score():
     l_score = []
-    cursor = mongo.db.flappy_score.find({})
-    for score in cursor:
-            tmp = score
-            l_score.append(tmp)
-    return dumps(l_score)
+    try:
+        cursor = mongo.db.flappy_score.find({}).sort([("score",-1)])
+        for score in cursor:
+                tmp = score
+                l_score.append(tmp)
+        response = make_response(dumps(l_score), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    except:
+        return make_response(jsonify({'error':'the database doesn \'t appear to be available'}), 504)
 
 """
 implements player's score & name in the database
 """
-@app.route('/flappy_api/setscore/<string:player_name>/<int:score_val>', methods=['GET'])
-def set_score(player_name, score_val):
+@app.route('/flappy_api/setscore', methods=['POST'])
+def set_score():
+    print (request)
+    if not request.get_json:
+        make_response(jsonify({'error':'No Json find'}), 404)
     try:
-        mongo.db.flappy_score.insert({"score":score_val, "name":player_name})
-        return jsonify("response:OK")
+        data = request.get_json()
+        mongo.db.flappy_score.insert({"score":data['score'], "name":data['playerName']})
+        response = make_response(jsonify({'response':'Insertion done'}), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
     except:
-        return make_response(jsonify({'error':'the database doesn \'t appear to be available'}), 504)
+        print (sys.exc_info()[0])
+        response = make_response(jsonify({'error':'the database doesn \'t appear to be available'}), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 if __name__ == '__main__':
     # TODO: not suitable for prod environment
